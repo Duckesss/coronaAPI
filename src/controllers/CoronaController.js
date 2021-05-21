2024b12b3b83587589a419bcd23c6c015ba7242d
Mateus Henrique <mateus.magalhaes@upflow.me>
2021-05-20 21:51:36 -0300

    Adição de novo método para atender a questão 02

--- src/controllers/CoronaController.js
+++ src/controllers/CoronaController.js
@@ -8,3 +8,15 @@ exports.getSixMonths = async (req,res) => {
     const dadosCorona = await CoronaService.getCases(country,initialDate,finalDate)
     return res.status(200).json(dadosCorona.data)
 }
+
+exports.getWeek = async (req,res) => {
+    const country = 'brazil'
+    let {qtdWeeks} = req.params  // qtd de semanas pra trás
+    qtdWeeks = Number(qtdWeeks)
+    const date = new Date()
+    const initialDate = format( sub(date,{weeks: qtdWeeks+1}), "yyyy-MM-dd" )
+    const finalDate = format( sub(date,{weeks: qtdWeeks, days: 1}) ,"yyyy-MM-dd")
+    console.log(initialDate,finalDate)
+    const dadosCorona = await CoronaService.getCases(country,initialDate,finalDate)
+    return res.status(200).json(dadosCorona.data)
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ const routes = Router()
 
 routes.get("/ping", (req,res) => res.send("pong"))
 routes.get("/lastSixMonths", CoronaController.getSixMonths)
+routes.get("/getWeek/:qtdWeeks", CoronaController.getWeek)
 
 module.exports = routes
